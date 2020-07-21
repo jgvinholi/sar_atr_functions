58a16c932e9d7e5bf58058477b87e8c0a95e25aa
root <jgvinholi@gmail.com>
2020-07-21 20:43:33 +0000

    .

--- twostepsdetect_functions.py
+++ twostepsdetect_functions.py
@@ -547,12 +547,16 @@ def detection_perfomance(model_conv, model_classconv, Pred, detect_thresh, class
   if save:
     Final_mask = bwimg(os.path.join(datab_imgs_path, img_name + ".jpg")).astype(np.uint8)
     Final_mask = np.repeat( np.reshape(Final_mask, (Final_mask.shape[0], Final_mask.shape[1], 1) ), repeats = 3, axis = 2 )
-    # Draw true positives
+    # Draw true detections
     for i, cluster in enumerate( detected_targets):
-      Final_mask = cv2.circle(Final_mask, (int(cluster[1]), int(cluster[0])), radius = 10, color = (50, 255, 0), thickness = 2)
-    # Draw false positives
+      Final_mask = cv2.circle(Final_mask, (int(cluster[1]), int(cluster[0])), radius = 14, color = (50, 255, 0), thickness = 2)
+    # Draw false alarms
     for i, cluster in enumerate( false_positives ):
-      Final_mask = cv2.circle(Final_mask, (int(cluster[1]), int(cluster[0])), radius = 10, color = (255, 0, 50), thickness = 2)
+      Final_mask = cv2.line(Final_mask, pt1 = (int(cluster[1])-12, int(cluster[0]-12)), pt2 = (int(cluster[1])+12, int(cluster[0]+12)), color = (255, 0, 50), thickness = 2)
+      Final_mask = cv2.line(Final_mask, pt1 = (int(cluster[1])-12, int(cluster[0]+12)), pt2 = (int(cluster[1])+12, int(cluster[0]-12)), color = (255, 0, 50), thickness = 2)
+    # Draw undetected targets
+    for i, cluster in enumerate( undetected_targets ):
+      Final_mask = cv2.rectangle(Final_mask, pt1 = (int(cluster[1])-12, int(cluster[0]-12)), pt2 = (int(cluster[1])+12, int(cluster[0]+12)), color = (255, 255, 20), thickness = 2)
     plt.imshow(Final_mask)
     # saveimg(Final_mask, os.path.join(datab_imgs_path, "predictions/" + img_name + "_dbscan_twosteps_prediction.jpg") )
     # np.savetxt(os.path.join(datab_imgs_path, "predictions/" + img_name + "_cluster_centers_prediction.txt"), clusters_centers, "%d" )
